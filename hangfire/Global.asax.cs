c9718c223796dfafcc6e3372b4cf93b31108e9dc
andrianarivonymiharifeno <feno.miary@outlook.fr>
2023-10-21 23:23:02 +0200

    first sample

--- hangfire/Global.asax.cs
+++ hangfire/Global.asax.cs
@@ -1,5 +1,7 @@
+using Hangfire;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -11,13 +13,28 @@ namespace hangfire
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            Hangfire.GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Server=FENO-PC;Database=HangFire;User Id=sa;Password=sql2019;TrustServerCertificate=True;");
+
+            yield return new BackgroundJobServer();
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            HangfireAspNet.Use(GetHangfireServers);
+
+            // Let's also create a sample background job
+            BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire!"));
         }
     }
 }
---
+++ hangfire/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.SqlServer;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(hangfire.Startup))]
+
+namespace hangfire
+{
+    public class Startup
+    {
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Data Source=FENO-PC;Initial Catalog=Hangfire;User ID=sa;Password=sql2019");
+
+            yield return new BackgroundJobServer();
+        }
+        public void Configuration(IAppBuilder app)
+        {
+            // Pour plus d'informations sur la configuration de votre application, visitez https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseHangfireAspNet(GetHangfireServers);
+            app.UseHangfireDashboard();
+
+            // Let's also create a sample background job
+            BackgroundJob.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+        }
+    }
+}
